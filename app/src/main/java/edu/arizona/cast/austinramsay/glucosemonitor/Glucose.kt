348251d1286cc5da80acee2379d29c63a1c0674e
Austin Ramsay <austinramsay@gmail.com>
2020-10-04 21:32:12 -0700

    Implemented the 'Glucose' data class. Implemented glucose level calculations and corresponding event handlers/observer callbacks. Report is output with corresponding status for each calculated value (hypoglycemic, normal, abnormal). Input field colors now correspond to calculated values (green if acceptable, red otherwise). Clear button is functional (clears input fields and report output). Implemented shared view model between main activity and input view fragment.

--- app/src/main/java/edu/arizona/cast/austinramsay/glucosemonitor/Glucose.kt
+++ app/src/main/java/edu/arizona/cast/austinramsay/glucosemonitor/Glucose.kt
@@ -8,6 +8,54 @@ data class Glucose(val date: LocalDate,
                    val lunch: Int,
                    val dinner: Int) {
 
+    val fastingStatus: String?
+    val breakfastStatus: String?
+    val lunchStatus: String?
+    val dinnerStatus: String?
+
+    companion object {
+        const val STATUS_NORMAL = "Normal"
+        const val STATUS_ABNORMAL = "Abnormal"
+        const val STATUS_HYPOGLYCEMIC= "Hypoglycemic"
+    }
+
+    // Upon instance creation, calculate the status properties using the provided glucose numbers
+    // If fasting levels are less than 70 or above 100 the levels are abnormal
+    // For between meals, values should be between 70 and 140
+    init {
+        // Set the fasting status
+        fastingStatus = if (fasting < 70) {
+            STATUS_HYPOGLYCEMIC
+        } else if (fasting >= 70 && fasting < 100) {
+            STATUS_NORMAL
+        } else {
+            STATUS_ABNORMAL
+        }
+
+        breakfastStatus = if (breakfast < 70) {
+            STATUS_HYPOGLYCEMIC
+        } else if (breakfast > 140) {
+            STATUS_ABNORMAL
+        } else {
+            STATUS_NORMAL
+        }
+
+        lunchStatus = if (lunch < 70) {
+            STATUS_HYPOGLYCEMIC
+        } else if (lunch > 140) {
+            STATUS_ABNORMAL
+        } else {
+            STATUS_NORMAL
+        }
+
+        dinnerStatus = if (dinner < 70) {
+            STATUS_HYPOGLYCEMIC
+        } else if (dinner > 140) {
+            STATUS_ABNORMAL
+        } else {
+            STATUS_NORMAL
+        }
+    }
  }
 
 
--- app/src/main/java/edu/arizona/cast/austinramsay/glucosemonitor/MainActivity.kt
+++ app/src/main/java/edu/arizona/cast/austinramsay/glucosemonitor/MainActivity.kt
@@ -2,11 +2,51 @@ package edu.arizona.cast.austinramsay.glucosemonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var outputView: TextView
+    private lateinit var clearButton: Button
+    private lateinit var historyButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        outputView = findViewById(R.id.output_view)
+        clearButton = findViewById(R.id.clear_button)
+        historyButton = findViewById(R.id.history_button)
+
+        // Get the shared view model
+        val model: SharedViewModel by viewModels()
+
+        // View model 'glucose' observer event handler
+        model.glucose.observe(this, Observer<Glucose> { newGlucose ->
+
+            if (newGlucose != null) {
+                val date = newGlucose.date
+                val dateStr = date.format(DateTimeFormatter.ofPattern("EEEE MMM dd, YYYY"))
+                outputView.text = String.format(
+                    "Report for %s:\n\nFasting: %s\nBreakfast: %s\nLunch: %s\nDinner: %s",
+                    dateStr,
+                    newGlucose.fastingStatus,
+                    newGlucose.breakfastStatus,
+                    newGlucose.lunchStatus,
+                    newGlucose.dinnerStatus)
+            }
+
+        })
+
+        // Clear button event handler
+        clearButton.setOnClickListener {
+            model.updateGlucose(null)
+
+            outputView.text = null
+        }
     }
 }
